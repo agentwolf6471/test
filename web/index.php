217704ac5534a8e4574ef55adcdb7e78ad2a260e
agentwolf6471 <idcidc6471@gmail.com>
2019-05-19 21:16:24 +0800

    changin

--- web/index.php
+++ web/index.php
@@ -1,16 +1,22 @@
  <!DOCTYPE html>
 <html> 	
-<head>
-	<link rel="stylesheet" type="text/css" href="homescreen.css"> 
-	<link rel="stylesheet" type="text/css" href="reset.css"> 
-	<title>website</title>
-</head>
+	<head>
+		<link rel="stylesheet" type="text/css" href="homescreen.css"> 
+		<link rel="stylesheet" type="text/css" href="reset.css"> 
+		<title>website</title>
+	</head>
 
-<body>
-<div id="home-screen">
-<?php
-echo hello
-?>
-</div>
-</body>
+	<body>
+		<div id="home-screen">
+			 <h1>Home</h1>
+			 <div>
+				<form action="login.php" method="POST">
+					<input id="username-form" type="text" value="username" placeholder="Username">
+					<input id="password-form" type="text" value="password" placeholder="password">
+					<input id="button-form" type="submit" value="submit">
+				</form>
+				<button onclick="location.href='register.php'" type="button"> Register now</button>
+			 </div>
+		</div>
+	</body>
 </html> 
