7dda715c7ebf2698235cccfb0cd6444306ae5f7c
agentwolf6471 <idcidc6471@gmail.com>
2019-05-19 19:48:36 +0800

    changin

--- web/index.php
+++ web/index.php
@@ -1,11 +1,15 @@
  <!DOCTYPE html>
 <html> 	
 <head>
-	 <link rel="stylesheet" type="text/css" href="inc/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="mystyle.css"> <link rel="stylesheet" type="text/css" href="inc/mystyle.css">
 	<title>website</title>
-</head>
+</head>10315487
 
 <body>
-
+<div class="home-screen">
+<?php
+header("header.html");
+?>
+</div>
 </body>
 </html> 
