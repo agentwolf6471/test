eb25474db8f3d1ec0af35b85186673e74a2fd5da
agentwolf6471 <idcidc6471@gmail.com>
2019-05-19 21:21:48 +0800

    changin

--- web/index.php
+++ web/index.php
@@ -9,13 +9,14 @@
 	<body>
 		<div id="home-screen">
 			 <h1>Home</h1>
-			 <div>
+			 <div class="login-page">
 				<form action="login.php" method="POST">
 					<input id="username-form" type="text" value="username" placeholder="Username">
 					<input id="password-form" type="text" value="password" placeholder="password">
 					<input id="button-form" type="submit" value="submit">
 				</form>
 				<button onclick="location.href='register.php'" type="button"> Register now</button>
+				
 			 </div>
 		</div>
 	</body>
